fcc4051f8b6f5c09aa60bca49645dd7ed5c976d0
Renee Shiyan Liu <reneesyliu@gmail.com>
2022-11-04 11:46:39 -0700

    Data cleaning done

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -38,6 +38,7 @@ def go(args):
         description=args.output_description,
     )
     artifact.add_file("clean_sample.csv")
+    logger.info("Uploading the cleaned artifact to wandb")
     run.log_artifact(artifact)
     ######################
 
